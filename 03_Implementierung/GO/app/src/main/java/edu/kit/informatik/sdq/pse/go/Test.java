973732c3a48744c08f619c5d90f9865db351aad8
Tina Str <stroessner.tinamaria@gmail.com>
2017-06-17 15:57:06 +0200

    Android-Projekt für Implementierung erstellt

---
+++ 03_Implementierung/GO/app/src/main/java/edu/kit/informatik/sdq/pse/go/Test.java
@@ -0,0 +1,18 @@
+package edu.kit.informatik.sdq.pse.go;
+
+/**
+ * Created by tina on 17.06.17.
+ * Test class for TexDoclet
+ */
+
+public class Test {
+
+
+    /**
+     * Diese Methode testet die Umsetzung von JAvadoc-Kommentaren in.tex-Files via TeXDoclet
+     * @param testParam ein Test-Parameter
+     */
+    public void testMethod(int testParam) {
+
+    }
+}
