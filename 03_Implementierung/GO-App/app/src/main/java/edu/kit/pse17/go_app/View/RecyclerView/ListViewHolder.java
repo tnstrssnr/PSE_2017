630c938ed99729e769857d7685abf2bc5f6671af
Tina Str <stroessner.tinamaria@gmail.com>
2017-06-22 15:40:37 +0200

    bisherige Paketstruktur ins Eintwurfsdokument aufgenommen

--- 03_Implementierung/GO-App/app/src/main/java/edu/kit/pse17/go_app/View/RecyclerView/ListViewHolder.java
+++ 03_Implementierung/GO-App/app/src/main/java/edu/kit/pse17/go_app/View/RecyclerView/ListViewHolder.java
@@ -6,6 +6,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import edu.kit.pse17.go_app.R;
+import edu.kit.pse17.go_app.View.RecyclerView.ListItems.ListViewHolder;
+
+import static android.support.v7.widget.RecyclerView.*;
 
 /**
  * Die Klasse erzeugt ViewHolder-Objekte, die die Datenobjekt für die RecyclerView enthalten
@@ -13,7 +16,7 @@ import edu.kit.pse17.go_app.R;
  * Created by tina on 17.06.17.
  */
 
-public class ListViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+public class SimpleListViewHolder extends ViewHolder implements View.OnClickListener {
 
     private final OnListItemClicked onListItemClicked;
 
@@ -30,17 +33,33 @@ public class ListViewHolder extends RecyclerView.ViewHolder implements View.OnCl
      */
     public ImageView icon;
 
+    public SimpleListViewHolder() {
+        super(null);
+
+        ViewHolder vh = new ViewHolder(null);
+        
+    };
+
     /**
      * Konstruktor
      * @param itemView View, in der die Items angezeigt werden sollen
      * @param onListItemClicked ClickListener für ListItems
      */
-    public ListViewHolder(View itemView, OnListItemClicked onListItemClicked) {
+    private SimpleListViewHolder(View itemView, OnListItemClicked onListItemClicked) {
         super(itemView);
         this.onListItemClicked = onListItemClicked;
         this.title = (TextView) itemView.findViewById(R.id.list_item_title);
         this.subtitle = (TextView) itemView.findViewById(R.id.list_item_subtitle);
         this.icon = (ImageView) itemView.findViewById(R.id.list_item_icon);
+    }
+
+    public SimpleListViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public static SimpleListViewHolder createViewHolder(View itemView, OnListItemClicked onListItemClicked) {
+        return new SimpleListViewHolder(itemView, onListItemClicked);
     }
     @Override
     public void onClick(View v) {
