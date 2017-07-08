097d132afe3ad4adace18bf8ed4ccb4bade9b6cd
Tina Str <stroessner.tinamaria@gmail.com>
2017-07-08 12:55:18 +0200

    Klassenbeschreibungen für Downstream Communication hinzugefügt

--- 03_Implementierung/GO-Tomcat/src/main/java/edu/kit/pse17/go_app/ClientCommunication/Downstream/FcmClient.java
+++ 03_Implementierung/GO-Tomcat/src/main/java/edu/kit/pse17/go_app/ClientCommunication/Downstream/FcmClient.java
@@ -1,17 +1,19 @@
 package edu.kit.pse17.go_app.ClientCommunication.Downstream;
 
+import com.google.gson.JsonObject;
+import org.apache.http.client.HttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.List;
 
 /**
  *Client-Klasse, die ein HTTP POST-Request an den FCM-Server schickt, wo die Nachricht wiederum an das User-Endgerät weitergeleitet wird.
  * dadurch kann der Server eine Nachricht an einen Client schicken, ohne dass dieser zuvor den Server angesprochen haben muss.
  *
  * Die Methoden der Klasse werden aufgerufen von den Observer-Klassen der Anwendung. Dabei werden die Nachrichten, die an die Clients gesendet werden müssen
- * sowie die Addressaten bereits in den aufrufenden Methoden bestimmt. Diese klasse muss sich nur um das eingentliche Senden der HTTP-reuwests kümmern, der
+ * sowie die Addressaten bereits in den aufrufenden Methoden bestimmt. Diese klasse muss sich nur um das eingentliche Senden der HTTP-Requests kümmern, der
  * Inhalt der Nachricht spielt dabei keine Rolle.
- *
- * Das ansprechen der Fcm-Server-Api wird mithilfe des Frameworks Retrofit realisiert.
  *
  */
 public class FcmClient {
@@ -22,27 +24,26 @@ public class FcmClient {
     private static final String BASE_URL = "https://fcm.googleapis.com/";
 
     /**
-     * Rest-Schnittstelle des FCM Servers, die vom Retrofit Framework angesprochen werden kann
+     * Ein Http-Client, der für das Senden der HTTP-Requests zuständig ist. Die Konfiguration des HttpClients wird bei der Erstellung des FcmClient-Objekts
+     * vorgenommen. Die Konfiguration wird von dem Firebase Cloud Messaging Service vorgegeben.
      */
-    private final FcmApi fcmApi;
+    private HttpClient httpClient;
 
     /**
-     * Konstruktor
+     * Die Methode einen POST-Request an den FCM-Server, der diese an das User-Endgerät weiterleitet. Dafür wird der HttpClient
+     * der FcmClient-Instanz benutzt.
+     *
+     * Diese Methode wird von den Observer-Klassen aufgerufen, um die Änderungen, die dort behandlet wurden mithilde dieser Klasse
+     * an die Clients zu schicken.
+     * Es wird vorausgesetzt, dass die Daten und vor allem die InstanceIDs, die dieser Methode übergeben werden, gültig sind.
+     *
+     * @param data Dieses Objekt enthält die Daten, die an den Client geschickt werden sollen
+     * @param command Ein String, der anzeigt, um was für eine Nachricht es sich handelt, also zu welchem Serverereignis sie gehört. Dieser
+     *                String bestimmt, an welche Command-Klasse auf dem Client die Nachricht weitergeleitet wird.
+     * @param receiver Eine Liste mit den InstaceIds der Clients,an die die Nachricht geschickt werden soll. dabei muss es sich um gültige
+     *                 InstanceIds handeln, sonst kann die Methode nicht fehlerfrei ausgeführt werden.
      */
-    public FcmClient() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+    public void send(JsonObject data, String command, List<String> receiver) {
 
-        fcmApi = retrofit.create(FcmApi.class);
-
-    }
-
-    /**
-     * sendet eine Message an den FCM-Server, der diese an das User-Endgerät weiterleitet
-     */
-    public void send() {
-        fcmApi.sendDownStreamMessage();
     }
 }
