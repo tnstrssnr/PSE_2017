bbf36099d90b665f3e79949d7d87ec8773481961
Tina Str <stroessner.tinamaria@gmail.com>
2017-07-09 12:49:11 +0200

    createDoc.sh Skript angepasst. Beim Ausführen wird eine docs.tex erzeugt, die die Klassenbeschreibungen der App enthält. Diese müssen anschließend manuell in die Entwurf.tex eingefügt werden

--- 02_Entwurf/createDoc.sh
+++ 02_Entwurf/createDoc.sh
@@ -5,23 +5,11 @@
 
 mkdir texdoclet_output
 
-rm texdoclet_output/Entwurf.aux
-
 javadoc -docletpath TeXDoclet.jar \
 	-doclet org.stfm.texdoclet.TeXDoclet \
 	-noindex \
 	-tree \
 	-hyperref \
-	#-texsetup texdoclet_include/setup.tex \
-	#-texintro texdoclet_include/intro.tex \
-	#-texfinish texdoclet_include/finish.tex \
-	#-texinit texdoclet_include/preamble.tex \
-	-output texdoclet_output/Entwurf.tex \
-	-sourcepath ../03_Implementierung/GO-Tomcat/src/main/java \
-	-subpackages /edu/kit/pse17 \
+	-sourcepath ../03_Implementierung/GO-App/app/src/main/java \
+	-subpackages /edu \
  	-sectionlevel section
-
-cd texdoclet_output
-pdflatex Entwurf.tex
-pdflatex Entwurf.tex
-cp Entwurf.pdf ../
