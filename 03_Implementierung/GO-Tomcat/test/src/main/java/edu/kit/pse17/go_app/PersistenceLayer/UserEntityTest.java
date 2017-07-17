75d0d38da858d2337fa7a75f27441799496fc2ae
Unknown <FlorianN.Kroeger@ymail.com>
2017-07-17 15:53:28 +0200

    Erste JUnitstests
    Erste JUnittests in PersistenceLayer

---
+++ 03_Implementierung/GO-Tomcat/test/src/main/java/edu/kit/pse17/go_app/PersistenceLayer/UserEntityTest.java
@@ -0,0 +1,69 @@
+package main.java.edu.kit.pse17.go_app.PersistenceLayer;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by FlorianKroeger on 17.07.2017.
+ */
+public class UserEntityTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getUid() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setUid() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getInstanceId() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setInstanceId() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getName() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setName() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getEmail() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setEmail() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getGroups() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setGroups() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getRequests() throws Exception {
+    }
+
+    @org.junit.Test
+    public void setRequests() throws Exception {
+    }
+
+    /*@org.junit.Test
+    public void equals() throws Exception {
+    }*/
+
+}
