3297ad8f46523551993f1b365567b8120935431b
FlorianKroeger <FlorianN.Kroeger@ymail.com>
2017-07-24 19:41:06 +0200

    Projektstruktur Update+Dependencies hinzugefügt
    Teststruktur neu aufgesetzt, Dependencies für JUnit 5

--- 03_Implementierung/GO-Tomcat/tests/edu/kit/pse17/go_app/PersistenceLayer/GoEntityTest.java
+++ 03_Implementierung/GO-Tomcat/tests/edu/kit/pse17/go_app/PersistenceLayer/GoEntityTest.java
@@ -1,120 +1,122 @@
-package main.java.edu.kit.pse17.go_app.PersistenceLayer;
+package edu.kit.pse17.go_app.PersistenceLayer;
 
-/**
- * Created by FlorianKroeger on 17.07.2017.
- */
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class GoEntityTest {
-    @org.junit.Before
+    @Before
     public void setUp() throws Exception {
     }
 
-    @org.junit.After
+    @After
     public void tearDown() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void getID() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void setID() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
+    public void getGroup() throws Exception {
+    }
+
+    @Test
+    public void setGroup() throws Exception {
+    }
+
+    @Test
     public void getName() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void setName() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void getDescription() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void setDescription() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void getStart() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void setStart() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void getEnd() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void setEnd() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void getLat() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void setLat() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void getLon() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void setLon() throws Exception {
     }
 
-    @org.junit.Test
-    public void getGroupId() throws Exception {
-    }
-
-    @org.junit.Test
-    public void setGroupId() throws Exception {
-    }
-
-    @org.junit.Test
-    public void getOwner() throws Exception {
-    }
-
-    @org.junit.Test
-    public void setOwner() throws Exception {
-    }
-
-    @org.junit.Test
+    @Test
     public void getGoingUsers() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void setGoingUsers() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void getNotGoingUsers() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void setNotGoingUsers() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void getGoneUsers() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
     public void setGoneUsers() throws Exception {
     }
 
-    @org.junit.Test
+    @Test
+    public void getOwner() throws Exception {
+    }
+
+    @Test
+    public void setOwner() throws Exception {
+    }
+
+    @Test
     public void equals() throws Exception {
     }
 
-    /*@org.junit.Test
+    /*@Test
     public void hashCode() throws Exception {
     }*/
-
 
 }
