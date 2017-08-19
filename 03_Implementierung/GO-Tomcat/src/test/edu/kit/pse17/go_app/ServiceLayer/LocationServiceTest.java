45432d8cd4bf2627d9ef6db1a187a5c3f73c0e78
FlorianKroeger <FlorianN.Kroeger@ymail.com>
2017-08-19 21:04:04 +0200

    Kleine Ergäzung im TexDokument+ DBScanTest und LocServTest(nichtoperable

---
+++ 03_Implementierung/GO-Tomcat/src/test/edu/kit/pse17/go_app/ServiceLayer/LocationServiceTest.java
@@ -0,0 +1,67 @@
+package edu.kit.pse17.go_app.ServiceLayer;
+
+import edu.kit.pse17.go_app.PersistenceLayer.GoEntity;
+import edu.kit.pse17.go_app.PersistenceLayer.clientEntities.Go;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+public class LocationServiceTest {
+
+    private LocationService testLocationService;
+
+    private GoEntity testEntity;
+
+    private GoService mockGoService;
+
+    private Map<Long, LocationService> mockMap;
+
+    private Long testLat;
+    private Long testLon;
+    private Long testGoId;
+    private String testUserId;
+
+    private Go testGo;
+
+    @Before
+    public void setUp() throws Exception {
+        this.testGoId = new Long(3);
+        this.testLon = new Long(5);
+        this.testLat = new Long(5);
+        this.testUserId = new String ("testUser");
+
+        this.testGo = new Go();
+        this.testEntity = new GoEntity();
+        this.mockGoService = Mockito.mock(GoService.class);
+        this.testLocationService = new LocationService(testEntity);
+        this.mockMap = Mockito.mock(Map.class);
+        Mockito.when(GoService.getGoById(testGoId)).thenReturn(testEntity);
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void setUserLocation() throws Exception {
+
+
+        testLocationService.setUserLocation(testGoId, testUserId, testLat, testLon);
+    }
+
+    @Test
+    public void getGroupLocation() throws Exception {
+    }
+
+    @Test
+    public void removeGo() throws Exception {
+    }
+
+}
